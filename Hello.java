9b166409ebeae903396d26934b258f5a37d6ea65
Jimmy George K <87347624+JimmyGeorgeK@users.noreply.github.com>
2022-04-21 10:17:08 +0530

    UpdateHello.java

--- Hello.java
+++ Hello.java
@@ -4,9 +4,10 @@ class HelloWorld
     {
       System.out.println("welcome to Java 8 Stream");
         System.out.println("hi I am fine");
-        int a,b,c;
+        int a,b,c,d;
         a=10;
         b=20;
         c=a+b;
+        d=a-b;
     }
 }
