c01a58644f2aeff9621f55b5f50acaaacc4461e1
Dell <anudotsoft@gmail.com>
2022-04-21 10:34:32 +0530

    basic eclipse project

---
+++ GitHubProjectExample/src/main/java/com/basic/code/AdditionExample.java
@@ -0,0 +1,14 @@
+package com.basic.code;
+
+public class AdditionExample {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=5;
+		c=a+b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ GitHubProjectExample/src/main/java/com/basic/code/HelloWorldExample.java
@@ -0,0 +1,11 @@
+package com.basic.code;
+
+public class HelloWorldExample {
+
+	public static void main(String[] args) {
+		System.out.println("hi i am from chennai");
+		System.out.println("I learn java");
+
+	}
+
+}
